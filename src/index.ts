cdedb69e245037dec0a4c692db5a70f7a115e55a
cosme098 <cosmeforeve@gmail.com>
2023-08-16 14:08:26 -0300

    refactor: logs

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,30 @@ import csrf from "csrf";
 import process from "process";
 import os from "os";
 import cluster from "cluster";
+import pino from "pino";
+const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+
 const app = express();
 
 const _csrf = new csrf();
 
 // app.use((req, res, next) => {
 //   if (req.url === "/csrf/token") return next();
-
 //   if (!req.get("token_CSRF")?.toString()) {
 //     res.status(400).json({ error: "Error request token CSRF" });
 //     return;
 //   }
-
 //   const rt = _csrf.verify(
 //     "asdasodasdiuybaskjd",
 //     req.get("token_CSRF")!.toString()
 //   );
-//   console.log(rt);
-
 //   rt ? next() : res.status(400).json({ error: "Error request token CSRF" });
 // });
 
@@ -35,23 +41,21 @@ const _csrf = new csrf();
 if (cluster.isPrimary) {
   const cpus = os.cpus().length / 2;
 
-  console.log(`set up three of:${cpus}`);
+  logger.info(`🌿  Starting ${cpus} forks`);
 
   for (let index = 1; index <= cpus; index++) {
-    cluster.fork({ EQ: index });
+    cluster.fork({ CPUI: index });
   }
 } else {
   app.post("/test", (req, res) => {
     // console.log(req.body);
     // console.log(req.headers);
-
-    console.log(process.env.EQ);
-
+    logger.info(`CPU:${process.env.CPUI}`);
     res.status(200);
     res.send();
   });
 
   http.createServer(app).listen(3000, () => {
-    console.log("running at 3000");
+    logger.info(`Running at 3000`);
   });
 }
